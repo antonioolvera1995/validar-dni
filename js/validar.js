b0098d096da4f00a93bf72086067a0cd999eb4fe
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-02 10:16:40 +0200

    commit-2

--- js/validar.js
+++ js/validar.js
@@ -1,7 +1,27 @@
 arrayLeters = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+arrayLetersNie = ['X', 'Y', 'Z']
 
+let block = false;
 
+function enter(e) {
 
+    if (e.keyCode === 13 && !e.shiftKey) {
+        e.preventDefault();
+        validar();
+
+        if (!block) {
+            document.getElementById("button").classList.add("on-button");
+            document.getElementById("button").style.backgroundColor = "#ffff";
+            block = true;
+            setTimeout(function () {
+                block = false;
+                document.getElementById("button").classList.remove("on-button");
+                document.getElementById("button").style.backgroundColor = "rgba(252, 252, 252, 0.582)";
+            }, 300);
+        }
+    }
+
+}
 
 
 
@@ -11,7 +31,7 @@ function validar() {
     let text = document.getElementById("msg");
 
     if (et.length === 9) {
-        validar2(et)
+        validar1(et)
     } else {
 
         text.innerHTML = 'Dni no válido';
@@ -20,6 +40,42 @@ function validar() {
     }
 
 }
+
+
+function validar1(dni) {
+
+
+    for (let i = 0; i < 4; i++) {
+
+        switch (arrayLetersNie[i]) {
+
+            case 'X':
+                dni = `0${dni.substring(1,9)}`;
+                break;
+
+            case 'Y':
+                dni = `1${dni.substring(1,9)}`;
+                break;
+
+
+            case 'Z':
+                dni = `2${dni.substring(1,9)}`;
+                break;
+
+
+            default:
+                break;
+        }
+
+    }
+
+
+
+
+    validar2(dni)
+}
+
+
 
 
 function validar2(dni) {
@@ -48,7 +104,7 @@ function validar2(dni) {
 
 function validar3(dni2) {
 
-    let dni = Number(dni2.substring(0,8));
+    let dni = Number(dni2.substring(0, 8));
     let text = document.getElementById("msg");
     let restDiv = dni % 23;
     let letra;
@@ -62,4 +118,4 @@ function validar3(dni2) {
         text.style.color = "red"
     }
 
-}   
+}
